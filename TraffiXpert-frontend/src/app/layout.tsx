b11fb934ba25c87b34843106b35ce85c9bb90817
WhiteDevil-45 <kaustubhmane12777@gmail.com>
2025-10-20 23:08:28 +0530

    Title Changed

--- TraffiXpert-frontend/src/app/layout.tsx
+++ TraffiXpert-frontend/src/app/layout.tsx
@@ -6,7 +6,7 @@ import { Toaster } from '@/components/ui/toaster';
 import { ThemeProvider } from '@/components/theme-provider';
 
 export const metadata: Metadata = {
-  title: 'VerdantFlow', // Or TraffiXpert
+  title: 'TraffiXpert', // Or TraffiXpert
   description: 'An advanced traffic management system.',
 };
 
